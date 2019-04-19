9cd2dff188552a65695193b9e1c0342993a0d1a1
gabrielomn <gabriel.nobrega@ccc.ufcg.edu.br>
2019-04-19 18:19:51 -0300

    subindo funcionabilidade da conversa do bot

--- src/app.js
+++ src/app.js
@@ -1,5 +1,1 @@
 const services = require('./services.js')
-
-function handleMessage(data){
-    services.handleMessage(data);
-}
--- src/services.js
+++ src/services.js
@@ -2,6 +2,8 @@ piii = require('./StinkyWordsFilter')
 const axios = require('axios')
 const idGabriel = "UHN2NCEF4"
 const SlackBot = require('slackbots')
+var runningChats = new Object();
+
 
 const bot = new SlackBot({
     token:'xoxb-610927659380-605202731841-SnjbA9emnZI5rdokgzbzCD8A',
@@ -28,24 +30,84 @@ bot.on('message', (data)=>{
         services.handleMessage(data)
     }
 })
-
+/*
 var handleMessage = function (data){
     
     if(!piii.has(data.text)){
         postToDuvidas(data.text);
-        returnMessage(data.user, 'Ta bom, vou enviar a duvida pro duvidas por vc ;)')
+        returnMessage(data.user, 'Olar, oq vc precisa?')
     }else{
-        chuckNorris()
         returnMessage(data.user, 'Sem palavrão. PALHAÇO')
     }
 }
+*/
+
+var handleMessage = function (data){
+    
+    if(!piii.has(data.text)){
+        id = data.user
+        if(!runningChats.hasOwnProperty(id)){
+            returnMessage(data.user, 'Olar, oq vc precisa?')
+            runningChats[data.user] = handleDuvida
+        }else{
+            runningChats[data.user](data)
+        }
+    }else{
+        returnMessage(data.user, 'Sem palavrão. PALHAÇO')
+    }
+}
+
+
+var handleDuvida = function(data){
+    returnMessage(data.user, 'Certo, que tipo de dúvida vc tem?\n' + listaCategoriasDuvidas() + "\ndigita apenas o número correspondente plz.")
+    runningChats[data.user] = lidaComTipoDeDuvida
+}
+
+var lidaComTipoDeDuvida = (data) => {
+    if(iUnderstoodTheDoubt(data.text)){
+        var categoria = data.text
+        returnMessage(data.user, 'Certo, manda a mensagem que eu encaminho para o dúvidas para vc')
+        runningChats[data.user] = function(data){
+            let mensagem = `DUVIDA SOBRE ${categorizer(categoria) + "\n" + data.text}` 
+            postToDuvidas(mensagem)
+        }
+    }
+}
+
+var categorizer = function(value){
+    if(value == 1){
+        return "Orientação a objeto"
+    }else if(value == 2){
+        return "Manipulação de coleções"
+    }else if(value == 3){
+        return "Testes"
+    }else if(value == 4){
+        return "Cronogramas"
+    }else if(value == 5){
+        return "Outros"
+    }
+    
+}
+
+var iUnderstoodTheDoubt = (text) => {
+    let expressaoEsperada = /(^[0-5]{1}$)/
+    return expressaoEsperada.test(text)
+}
+
+
+var listaCategoriasDuvidas = function(){
+    let saida = "1. Orientação a objeto\n2.Manipulação de coleções\n3.Testes\n4.Cronogramas\n5.Outros\n Digaí o tipo de dúvida ;)"
+    return saida
+}
+
+
 
 var returnMessage = function (id, resposta){
     bot.postMessageToUser(getUserNameById(id), resposta)  
 }
 
 var postToDuvidas = function (message){
-    bot.postMessageToChannel('duvidas', "------ DUVIDA ------\n" + message)
+    bot.postMessageToChannel('duvidas',message)
 }
 
 var postToGeneral = function(message){
