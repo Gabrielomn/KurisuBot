8eb22db0af554855583648f40832dedd30c32641
gabrielomn <gabriel.nobrega@ccc.ufcg.edu.br>
2019-07-08 11:38:58 -0300

    preparing for deploy

--- models/database.js
+++ models/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect("mongodb://localhost/database").then(() => {
+mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log('Database connected')
 }).catch(error => {
   console.log('Erro: ' + error)
