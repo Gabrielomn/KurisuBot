e7f83ce1fc6a5d3c17452923f962694c8471fdde
João Felipe <39709263+ja1felipe@users.noreply.github.com>
2019-07-10 22:42:17 -0300

    WorkSpace schema

---
+++ models/Workspace.js
@@ -0,0 +1,19 @@
+const mongo = require('./database')
+const WorkSpaceSchema = mongo.Schema({
+    adm: {
+        type: Array,
+        require: true
+    },
+    workspace:{
+        type: String,
+        require: true
+    },
+    channelPost:{
+        type: String,
+        require: true
+    }
+})
+
+mongo.model('workspaces', WorkSpaceSchema)
+const workspaces = mongo.model('workspaces')
+module.exports = workspaces
